ddec5dafeac2c85d07081256b9a83d072548cccc
Stephen Aranda <aranda.stephen88@gmail.com>
2021-10-30 07:13:47 -0500

    added server.use for express helmet cors cookieParser

--- api/server.js
+++ api/server.js
@@ -2,3 +2,15 @@ const express=require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const helmet = require('helmet')
+
+const {restricted} = require('../api/auth/auth-middleware')
+
+const authRouter = require('../api/auth/auth-router')
+const usersRouter = require('../api/users/user-router')
+
+const server = express()
+
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
+server.use(cookieParser())
