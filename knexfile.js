20437aa44683cbaef220cea7b43835d806b747ad
Stephen Aranda <aranda.stephen88@gmail.com>
2021-10-29 19:11:58 -0500

    added require dotenv and pg

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,12 @@
 // Update with your config settings.
+require('dotenv').config()
+
+const pg = require('pg')
+if (process.env.DATABASE_URL){
+  pg.defaults.ssl={rejectUnauthorized:false}
+}
+
+
 
 module.exports = {
 
