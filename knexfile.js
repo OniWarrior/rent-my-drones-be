ec2e490240a95a20091f2fe006c9fc95530d6905
Stephen Aranda <aranda.stephen88@gmail.com>
2021-10-29 19:17:51 -0500

    completed modul.exports

--- knexfile.js
+++ knexfile.js
@@ -15,42 +15,19 @@ const sharedConfig={
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    ...sharedConfig,
+    connection: process.env.DATABASE_URL 
+    
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  testing:{
+    ...sharedConfig,
+    connection:process.env.TESTING_DATABASE_URL
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  production:{
+    ...sharedConfig,
+    connection:process.env.DATABASE_URL,
+    pool:{min:2,max:10}
   }
+  
 
 };
