239fd2a2ee1cda6d5c392f2b41259cf34aa476c5
Stephen Aranda <aranda.stephen88@gmail.com>
2021-10-30 08:26:48 -0500

    added logic to verify token 8-21

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -4,7 +4,22 @@ const User = require('../users/user-model')
 
 const restricted=(req,res,next)=>{
     const token = req.headers.authorization
-    
+
+    if(!token){
+        res.status(401).json("Token required")
+    }
+    else{
+        jwt.verify(token,JWT_SECRET,(err,decoded)=>{
+            if(err){
+                res.status(401).json('Token invalid')
+            }
+            else{
+                req.decodedToken = decoded
+                next()
+            }
+        })
+    }
+
 
 }
 
