4cbed2aa0a805745d23f879fd14a151292266803
Stephen Aranda <aranda.stephen88@gmail.com>
2021-10-30 07:31:32 -0500

    added index to launch server

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000 // This is the fallback for testing
+
+const server = require('./api/server')
+
+server.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
