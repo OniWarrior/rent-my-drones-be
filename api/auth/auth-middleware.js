b341b6389c69676a52d150315de0dc56aa308145
Stephen Aranda <aranda.stephen88@gmail.com>
2021-10-30 08:22:13 -0500

    look at the auth header

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -2,3 +2,9 @@ const {JWT_SECRET} = require('../secrets/secret')
 const jwt = require('jsonwebtoken')
 const User = require('../users/user-model')
 
+const restricted=(req,res,next)=>{
+    const token = req.headers.authorization
+    
+
+}
+
