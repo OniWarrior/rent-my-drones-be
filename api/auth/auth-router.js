dbb3d50de84fe953252326874c6c5d5c0103d5ff
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-04 12:23:29 -0500

    created consts for middleware

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,4 +2,9 @@ const router = require('express').Router()
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const JWT_SECRET = require('../secrets/secret')
-
+const { checkUsernameFree } = require('./auth-middleware')
+const {
+    checkUsernameExists,
+    checkUsernameFree,
+    checkForMissingUsernamePassword
+} = require('./auth-middleware')
