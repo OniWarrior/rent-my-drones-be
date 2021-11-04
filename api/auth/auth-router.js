a87f5487479d9227b65e9fcb557370e9f10b7efb
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-04 12:38:49 -0500

    create const for added user

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -15,7 +15,7 @@ router('/Signup',checkUsernameFree,checkForMissingUsernamePassword,async(req,res
         let user = req.body
         let rounds = parseInt(process.env.ROUNDS)
         let hash = bcrypt.hashSync(user.password,rounds)
-
+        const addedUser = await User.addUser(user)
     }catch(err){
 
     }
