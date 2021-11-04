6218920dc634f7a5ad37dd8aba80c81adb3f677f
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-04 16:52:43 -0500

    created blank function for available path

--- api/users/user-router.js
+++ api/users/user-router.js
@@ -1,3 +1,9 @@
 const router = require('express').Router()
 const Drone = require('../drones/drones-model')
 const {default:jwtDecode} = require('jwt-decode')
+const { restricted } = require('../auth/auth-middleware')
+
+
+router('/available',restricted,(req,res,next)=>{
+    
+})
