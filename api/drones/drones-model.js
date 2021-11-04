18b48493e33852a8e2e937f57f1d47bff4fcb566
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-04 13:33:20 -0500

    created function to retrieve all available drones

--- api/drones/drones-model.js
+++ api/drones/drones-model.js
@@ -1,1 +1,15 @@
 const db = require('../data/dbConfig')
+
+function available(){
+    return db('drones')
+           .returning(['drone_id',
+                       'drone_name',
+                       'drone_description',
+                       'drone_cost',
+                       'drone_image',
+                       'drone_isRented'])
+            .where('drone_isRented',0)
+            .orderBy('drone_id')
+
+}
+
