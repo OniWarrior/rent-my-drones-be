5c6b15b8140a6c3efc2f7696a2de4dc75b35bdb5
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-21 07:39:42 -0600

    deleted findByToken

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -1,13 +1,7 @@
 const db = require('../data/dbConfig')
 
 
-async function findByToken(token){
-    const account = await db('users as u')
-                          .returning(['u.user_id','u.username','u.password'])
-                          .where('u.password',token)
-                          .first()
-    return account
-}
+
 
 async function findByUsername(username){
     const account = await db('users')
