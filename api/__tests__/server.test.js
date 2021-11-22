2acf00027c3f5c189aac4a59fd8bf13a117d602f
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-22 09:01:23 -0600

    created before all, added a rollback

--- api/__tests__/server.test.js
+++ api/__tests__/server.test.js
@@ -1,3 +1,9 @@
 const request = require('supertest')
 const server = require('../server')
 const db = require('../data/dbConfig')
+
+// start with a fresh unaltered db before doing main tests
+beforeAll(()=>{
+    await db.migrate.rollback()
+    
+})
