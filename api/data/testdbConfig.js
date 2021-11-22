d31daaa751ccb704a683e8a07e20337c468a3654
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-22 11:54:44 -0600

    deleted unnecessary file

--- api/data/testdbConfig.js
+++
@@ -1,5 +0,0 @@
-const knex = require('knex')
-const config = require('../../knexfile')
-const environment = process.env.TESTING_DATABASE_URL || 'testing'
-
-module.exports = knex(config[environment])
