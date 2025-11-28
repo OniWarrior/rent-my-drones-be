55d4d4f1ad5691b33d59f6f55bddd147bfad45f1
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-27 18:05:07 -0600

    added last name field

--- api/data/migrations/20211105105348_tables.js
+++ api/data/migrations/20211105105348_tables.js
@@ -6,6 +6,7 @@ exports.up = function (knex) {
             users.string('email', 128).notNullable().unique()
             users.string('password', 128).notNullable()
             users.string('first_name', 30).notNullable()
+            users.string('last_name', 30).notNullable()
         })
         .createTable('Drone', drones => {
             drones.increments('drone_id')
