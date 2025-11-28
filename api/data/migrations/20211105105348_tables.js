ec4f04ad1ff209fb000345b5551b1f8e301ff6da
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-27 18:03:10 -0600

    changed field to email

--- api/data/migrations/20211105105348_tables.js
+++ api/data/migrations/20211105105348_tables.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
     return knex.schema
         .createTable('User', users => {
             users.increments('user_id')
-            users.string('username', 128).notNullable().unique()
+            users.string('email', 128).notNullable().unique()
             users.string('password', 128).notNullable()
         })
         .createTable('Drone', drones => {
