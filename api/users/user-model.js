0cf90f11cc575fb5fdfc8ccc1184bf80ed228ead
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-01 15:56:32 -0600

    deleted unused query

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -39,16 +39,10 @@ async function addUser(user) {
     return addUser
 }
 
-async function findById(id) {
-    const account = await db('User')
-        .returning(['user_id', 'username', 'password'])
-        .where('user_id', id)
-        .first()
-    return account
-}
+
 
 module.exports = {
-    findById,
+
     addUser,
     findByEmail,
     checkIdentityByEmail
