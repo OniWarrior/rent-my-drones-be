11b4e75a72313e793d21d8e3e2efbad3ee937849
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-05 19:00:15 -0600

    added correct props to records

--- api/data/seeds/01-drones.js
+++ api/data/seeds/01-drones.js
@@ -4,35 +4,56 @@ const drones = [
         drone_name: 'Quad Copter',
         drone_description: 'Mini drone with four engines for mobility and stability',
         drone_cost: 300,
-        drone_image: 'https://images.unsplash.com/photo-1527977966376-1c8408f9f108?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80'
+        drone_image: 'https://images.unsplash.com/photo-1527977966376-1c8408f9f108?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80',
+        owner_id: 1,
+        drone_is_rented: false
+
+
+
     },
     {
 
         drone_name: 'Gray Quad Drone',
         drone_description: 'Mini drone with four engines for mobility and stability. Camera is attached under the main body.',
         drone_cost: 200,
-        drone_image: 'https://images.unsplash.com/photo-1542061986776-ccc39eb160ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80'
+        drone_image: 'https://images.unsplash.com/photo-1542061986776-ccc39eb160ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
+        owner_id: 1,
+        drone_is_rented: false
+
+
     },
     {
 
         drone_name: 'Mavic Pro',
         drone_description: 'Mini drone with four engines. Camera is attached under the main body.Black body',
         drone_cost: 200,
-        drone_image: 'https://images.unsplash.com/photo-1520870121499-7dddb6ccbcde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80'
+        drone_image: 'https://images.unsplash.com/photo-1520870121499-7dddb6ccbcde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80',
+        owner_id: 1,
+        drone_is_rented: false
+
+
     },
     {
 
         drone_name: 'Mavic Pro +',
         drone_description: 'Mini drone with four engines. Camera is attached under the main body.Body is stronger.',
         drone_cost: 500,
-        drone_image: 'https://images.unsplash.com/photo-1527441385177-3dad16222699?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'
+        drone_image: 'https://images.unsplash.com/photo-1527441385177-3dad16222699?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80',
+        owner_id: 1,
+        drone_is_rented: false
+
+
     },
     {
 
         drone_name: 'Mavic Pro ++',
         drone_description: 'Drone with four engines, and Panasonic DMC-GX7 camera. Camera is attached under the main body.Body is stronger.',
         drone_cost: 800,
-        drone_image: 'https://images.unsplash.com/photo-1473186639016-1451879a06f0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1105&q=80'
+        drone_image: 'https://images.unsplash.com/photo-1473186639016-1451879a06f0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1105&q=80',
+        owner_id: 1,
+        drone_is_rented: false
+
+
     }
 
 
