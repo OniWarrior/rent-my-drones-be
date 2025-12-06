8d4ca445fc824e18055a1d6133971e831baeee5c
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-06 14:59:11 -0600

    added desc comment along with vars for queries

--- api/users/owner-router.js
+++ api/users/owner-router.js
@@ -1,5 +1,4 @@
-import { restricted } from '../auth/auth-middleware';
-const { default: jwtDecode } = require('jwt-decode');
+
 
 /*
  * Author: Stephen Aranda
@@ -7,12 +6,23 @@ const { default: jwtDecode } = require('jwt-decode');
  * Desc  : endpoints that will be utilized by the owner user type.
  * */
 const router = require('express').Router();
+import { restricted } from '../auth/auth-middleware';
+const { default: jwtDecode } = require('jwt-decode');
+const Owner = require('./owner-model');
+const User = require('./user-model');
 
+/*
+ * /available-drones: endpoint that retrieves owner drones that are available to rent out.
+ * */
 router.get('/available-drones', restricted, async (req, res) => {
     try {
 
         // decode the token
         const decoded = jwtDecode(req.headers.authorization);
+
+
+
+
 
 
     } catch (err) {
