6728ea8a392ca70cbb627ba2c7d90034d30a9472
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-06 14:53:34 -0600

    added blank available drones endpoint

--- api/users/owner-router.js
+++ api/users/owner-router.js
@@ -1,8 +1,18 @@
+import { restricted } from '../auth/auth-middleware';
+
 /*
  * Author: Stephen Aranda
  * File  : owner-router.js
  * Desc  : endpoints that will be utilized by the owner user type.
  * */
 const router = require('express').Router();
+
+router.get('/available-drones', restricted, async (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+})
 
 export default router;
