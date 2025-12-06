83419589618a4a71060f1a9612ed116cbedf1f50
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-06 14:45:50 -0600

    added query to retrieve owners rented drones.

--- api/users/owner-model.js
+++ api/users/owner-model.js
@@ -8,6 +8,7 @@ const db = require('../data/dbConfig');
 
 /*
  * getOwnerAvailableDrones: retrieve all of the owner's drones that are available to be rented out
+ * @owner_id: param of the owner's id. Used to retrieve drones.
  * */
 const getOwnerAvailableDrones = async (owner_id) => {
     const drones = await db("Drone")
@@ -19,11 +20,34 @@ const getOwnerAvailableDrones = async (owner_id) => {
             'renter_id'
         )
         .where('owner_id', owner_id)
-        .first()
+        .where('drone_is_rented', false)
+
     return drones
 
 }
 
+/*
+ * getOwnerRentedDrones: retrieve all of the owner's drones that have been rented out
+ * @owner_id: param that is used to retrieve owner's drones.
+ * 
+ * */
+const getOwnerRentedDrones = async (owner_id) => {
+    const drones = await db('Drone')
+        .select('drone_id',
+            'drone_name',
+            'drone_description',
+            'drone_cost',
+            'drone_image',
+            'renter_id'
+        )
+        .where('owner_id', owner_id)
+        .where('drone_is_rented', true)
+    return drones
+
+}
+
+
 module.exports = {
-    getOwnerAvailableDrones
+    getOwnerAvailableDrones,
+    getOwnerRentedDrones
 }
