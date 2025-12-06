5f1f910eab46bbada1a93fafbd97b35b3be4135b
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-06 14:40:44 -0600

    added query to retrieve owners available drones

--- api/users/owner-model.js
+++ api/users/owner-model.js
@@ -5,3 +5,25 @@
  * */
 
 const db = require('../data/dbConfig');
+
+/*
+ * getOwnerAvailableDrones: retrieve all of the owner's drones that are available to be rented out
+ * */
+const getOwnerAvailableDrones = async (owner_id) => {
+    const drones = await db("Drone")
+        .select('drone_id',
+            'drone_name',
+            'drone_description',
+            'drone_cost',
+            'drone_image',
+            'renter_id'
+        )
+        .where('owner_id', owner_id)
+        .first()
+    return drones
+
+}
+
+module.exports = {
+    getOwnerAvailableDrones
+}
