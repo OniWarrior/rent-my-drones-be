b97058f595c4db5b73432456894249e2ce9ec564
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-07 20:18:49 -0600

    updated knexfile config

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,38 @@
 // Update with your config settings.
 require('dotenv').config()
 
-const pg = require('pg')
-if (process.env.DATABASE_URL){
-  pg.defaults.ssl={rejectUnauthorized:false}
-}
 
-const sharedConfig={
-  client:'pg',
-  migrations:{directory:'./api/data/migrations'},
-  seeds:{directory:'./api/data/seeds'}
+
+const sharedConfig = {
+  client: 'pg',
+  migrations: { directory: './api/data/migrations' },
+  seeds: { directory: './api/data/seeds' }
 }
 
 module.exports = {
-
   development: {
     ...sharedConfig,
-    connection: process.env.DEV_DATABASE_URL 
-    
+    connection: {
+      connectionString: process.env.DEV_DATABASE_URL,
+      ssl: false,
+    },
   },
-  testing:{
-    ...sharedConfig,
-    connection:process.env.TESTING_DATABASE_URL
-  },
-  production:{
-    ...sharedConfig,
-    connection:process.env.DATABASE_URL,
-    pool:{min:2,max:10}
-  }
-  
 
+  testing: {
+    ...sharedConfig,
+    connection: {
+      connectionString: process.env.TESTING_DATABASE_URL,
+      ssl: false,
+    },
+  },
+
+  production: {
+    ...sharedConfig,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      // Heroku Postgres wants SSL, but we relax cert check
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: { min: 2, max: 10 },
+  },
 };
